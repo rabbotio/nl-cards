5b222b2001b4b026616839be475a7de12703b358
katopz <katopz@gmail.com>
2019-02-05 18:35:54 +0700

    Fixed position

--- web/src/factory/AnswerFactory.js
+++ web/src/factory/AnswerFactory.js
@@ -13,6 +13,7 @@ const InnerContent = styled.div`
   align-self: center;
   margin-left: 16px;
   margin-right: 16px;
+  ${props => `display:${props.display === undefined || props.display !== null ? 'block' : 'none'}`}
 `
 
 const Desc = styled.div`
@@ -24,41 +25,52 @@ const Desc = styled.div`
   text-align: left;
 `
 
-const Bullets = styled.div`
-  display: flex;
-  flex: 0.4;
-  text-align: left;
-  font-size: 0.8em;
-
-  ul {
-    list-style: none;
-    margin-left: 0.5em;
-    padding-left: 0;
-    padding-top: 0.5em;
-  }
-
-  li {
-    padding-left: 1em;
-    text-indent: -1.7em;
-    line-height: 1.2em;
-    margin-block-start: -0.5em;
-    #min-height: 2.2em;
-    padding-top: 0.5em;
-    padding-bottom: 0.5em;
-  }
-
-  li:before {
-    content: '⭐';
-    padding-right: 5px;
-  }
-`
-
 const makeBullets = bullets => {
-  return `<ul>` + bullets.map(element => `<li>${element}</li>`).join('') + `</ul>`
+  return bullets ? `<ul>` + bullets.map(element => `<li>${element}</li>`).join('') + `</ul>` : ''
+}
+
+const FlexBullet = ({ left = null, right = null }) => {
+  const isDFN = left.join(',').indexOf('<dfn>') > -1
+  const Bullets = styled.div`
+    display: flex;
+    flex: 0.4;
+    text-align: left;
+    font-size: 0.8em;
+    justify-content: space-evenly;
+
+    ul {
+      list-style: none;
+      margin-left: 0.5em;
+      padding-left: 0;
+      padding-top: 0.5em;
+    }
+
+    li {
+      padding-left: 1em;
+      text-indent: -1.7em;
+      #line-height: 1.2em;
+      margin-block-start: -0.5em;
+      #min-height: 2.2em;
+      #padding-top: 0.5em;
+      padding-bottom: 0.5em;
+      ${isDFN ? 'margin-block-start: -1em' : ''};
+    }
+
+    li:before {
+      content: '⭐';
+      padding-right: 5px;
+    }
+  `
+  return (
+    <Bullets>
+      <InnerContent display={left}>{ReactHtmlParser(makeBullets(left))}</InnerContent>
+      <InnerContent display={right}>{ReactHtmlParser(makeBullets(right))}</InnerContent>
+    </Bullets>
+  )
 }
 
 export default class AnswerFactory {
-  static build ({ math, desc, bullets, image }) {
+  static build ({ math, desc, bullets, bullets_extra, image }) {
     const Image = styled.div`
       color: black;
       background-color: white;
@@ -88,9 +100,7 @@ export default class AnswerFactory {
             </MathJax.Context>
           </InnerContent>
         </Math>
-        <Bullets>
-          <InnerContent>{ReactHtmlParser(makeBullets(bullets))}</InnerContent>
-        </Bullets>
+        <FlexBullet left={bullets} right={bullets_extra} />
       </Flex>
     )
   }
